b97969016a4f74212876357631c21cd543d553cd
sandhyasipan <sandhyaswain70@gmail.com>
2022-05-22 20:52:34 +0530

    first programming

--- src/com/bridgelab/UC2_Empwage.java
+++ src/com/bridgelab/UC2_Empwage.java
@@ -1,5 +1,21 @@
 package com.bridgelab;
 
 public class UC2_Empwage {
+    public static void main (String[] args){
+        int isfulltime=1;
+        int emprateperhour=20;
+        int emphour=0;
+        int empwage=0;
+        int emptype = (int) (Math.random () * 10) % 2;
+        if (emptype == isfulltime) {
+            emphour = 8;
+        }else
+            emphour = 0;
+            empwage = emphour * emprateperhour;
+            System.out.println("empwage: " + empwage);
 
-}
+        }
+
+
+    }
+
